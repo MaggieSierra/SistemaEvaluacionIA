7e523857c157c718e4ecabf665620e7633a25cdc
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-12-09 13:53:01 -0600

    Se organizaron las vistas segun el rol

--- menu.php
+++ menu.php
@@ -1,10 +1,11 @@
 <ul class='nav nav-tabs'>
     <li class='nav-item'><a class='nav-link' href='index.php'>Inicio</a></li>
     <?php if($_SESSION['rol'] == 1){ ?>
-        <li class="nav-item"><a class="nav-link" href="evaluaciones.php">Evaluaciones</a></li>
-        <li class="nav-item"><a class="nav-link" href="calificaciones.php">Calificaciones</a></li>
+        <li class="nav-item"><a class="nav-link" href="vteacher/evaluaciones.php">Evaluaciones</a></li>
+        <li class="nav-item"><a class="nav-link" href="vteacher/calificaciones.php">Calificaciones</a></li>
+        <li class="nav-item"><a class="nav-link" href="vteacher/materias.php">Materias</a></li>
     <?php } else { ?>
-        <li class="nav-item"><a class="nav-link" href="materias.php">Materias</a></li>
+        <li class="nav-item"><a class="nav-link" href="vstudent/materias.php">Materias</a></li>
     <?php } ?>
     <li class="nav-item"><a class="nav-link" href="logout.php">Cerrar Sesion</a></li>
 </ul>
