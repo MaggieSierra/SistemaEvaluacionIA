25dfe9fcc3d54a2178c8582bcb39a710fa794639
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-12-11 01:37:58 -0600

    Crear evaluación funcionando

--- vteacher/crear_evaluacion.php
+++ vteacher/crear_evaluacion.php
@@ -32,14 +32,15 @@ foreach ($materias as $row) {
         <div class="row col-md-12" align="center" style="justify-content: center;">
             <h3>Nueva Evaluación</h3>
         </div>
+        <div id="response"></div>
         <div class="card">
             <div class="card-header"></div>
             <div class="card-body">
                 <div class="row" style="margin-bottom: 20px;">
                     <div class="col-md-4"><strong>Materia</strong></div>
                     <div class="col-md-8">
-                        <select name="id_materia" id="materia" class="form-control" style="display: inline-block;">
-                            <option value="0" selected disabled>Selección</option>
+                        <select name="id_materia" id="id_materia" class="form-control" style="display: inline-block;">
+                            <option value="" selected disabled>Selección</option>
                             <?=$list_materias;?>
                         </select>
                     </div>
@@ -47,11 +48,11 @@ foreach ($materias as $row) {
                 <div class="row" style="margin-bottom: 20px;">
                     <div class="col-md-4"><strong>Tema: </strong></div>
                     <div class="col-md-8">
-                        <input type="text" name="tema" class="form-control" required>
+                        <input type="text" name="tema" id="tema" class="form-control" required>
                     </div>
                 </div>
                 <button class="btn btn-success" onclick="add_pregunta();">+</button>
-                <button class="btn btn-success">Guardar</button>
+                <button class="btn btn-success" onclick="guardar_evaluacion();">Guardar</button>
             </div>
         </div><br>
         <div id="preguntas"></div>
@@ -65,6 +66,7 @@ foreach ($materias as $row) {
     <script type="text/javascript">
         agregar_preguntas = 0;
         count_preguntas = 0;
+        preguntas=[];
 
         function add_pregunta() {
             agregar_preguntas++;
@@ -75,7 +77,7 @@ foreach ($materias as $row) {
                                 +'<div class="row" style="margin-bottom: 20px;">'
                                 +' <div class="col-md-4"><strong>Pregunta</strong></div>'
                                 +'<div class="col-md-8">'
-                                +'  <input type="text" id="pregunta_'+agregar_preguntas+'" name="pregunta_'+agregar_preguntas+'" class="form-control" required>'
+                                +'  <input type="text" id="pregunta_'+agregar_preguntas+'" name="pregunta_'+agregar_preguntas+'" class="form-control" value="" required>'
                                 +' </div>'
                                 +' </div>'
                                 +' <div class="row" style="margin-bottom: 20px;">'
@@ -84,7 +86,7 @@ foreach ($materias as $row) {
                                 +'       <span class="text-warning">Nota: Separar las palabras clave por comas</span>'
                                 +'  </div>'
                                 +' <div class="col-md-8">'
-                                +'     <input type="text" id="pclaves_'+agregar_preguntas+'" name="pclaves_'+agregar_preguntas+'" class="form-control" required>'
+                                +'     <input type="text" id="pclaves_'+agregar_preguntas+'" name="pclaves_'+agregar_preguntas+'" value=""  class="form-control" required>'
                                 +'   </div>'
                                 +' </div>'
                                 +'</div>'
@@ -95,6 +97,83 @@ foreach ($materias as $row) {
         function quitar_pregunta(pregunta){
             $('#card_pregunta_'+pregunta).remove();
             
+        }
+
+        function guardar_evaluacion(){
+            if(validar_campos()){
+                validar_preguntas();
+                if(preguntas.length>0){
+                    let id_materia = document.getElementById("id_materia").value;
+                    let tema = document.getElementById("tema").value;
+
+                    $.ajax({
+                        url: "../php/crear_evaluacion.php",
+                        type: "POST",
+                        data:{
+                            id_materia: id_materia,
+                            tema: tema,
+                            preguntas: JSON.stringify(preguntas)
+                        }, success: function(data){
+                            if(data !="" && data > 0){
+                                $('#response').html('<span style="color:red"><strong>Evaluación guardada correctamente</strong></span>');
+                            }
+                        }
+                    });
+                }
+
+            }
+        }
+
+        function validar_campos(){
+            // Validar que nombre no sea vacio y solo admita letras y punto
+            if (document.getElementById('id_materia').value.trim().length == 0) {
+                alert("Selección la materia a la que se aplicará la evaluación");
+                document.getElementById('id_materia').focus();
+                return false;
+            } 
+
+            // Validar que nombre no sea vacio y solo admita letras y punto
+            if (document.getElementById('tema').value.trim().length == 0) {
+                alert("Agregue el tema de la evaluación");
+                document.getElementById('tema').focus();
+                return false;
+            } else if (!(/^([a-zA-Z\À-ÿ\á\é\í\ó\ú\ñ\ü\s](.?))+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test(document.getElementById('tema').value))) {
+                alert("Agregue un tema válido");
+                document.tema.focus();
+                return false;
+            }
+
+            if ( document.getElementById('id_materia').value.trim().length > 0 && (/^([a-zA-Z\À-ÿ\á\é\í\ó\ú\ñ\ü\s](.?))+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test(document.getElementById('tema').value))){
+                return true;
+            }
+            
+        }
+
+        function validar_preguntas(){
+            let datos = []; //guarda la pregunta con sus palabras clave
+            let add = false;
+            for(i=1; i<=agregar_preguntas; i++){
+                if(document.getElementById('pregunta_'+i+'')){
+                    if(document.getElementById('pregunta_'+i+'').value != "" && document.getElementById('pclaves_'+i+'').value != "" ){
+                        datos.push([document.getElementById('pregunta_'+i+'').value, document.getElementById('pclaves_'+i+'').value]);
+                    }else if(document.getElementById('pregunta_'+i+'').value != "" && document.getElementById('pclaves_'+i+'').value == ""){
+                        alert("Agregue las palabras clave");
+                        document.getElementById('pclaves_'+i+'').focus();
+                    }else{
+                        alert("Agregue la pregunta");
+                        document.getElementById('pregunta_'+i+'').focus();
+                    }
+                    add = true;
+                }
+            }
+
+            if(datos.length == 0 && !add){
+                alert("Agregue preguntas a la evaluación");
+            }
+
+            if(datos.length > 0){
+                preguntas = datos;
+            }
         }
     </script>
 </body>
