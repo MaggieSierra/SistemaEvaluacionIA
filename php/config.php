879b74ee7c5c2de90655853634aa8e2f1e57bc47
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-12-13 01:10:35 -0600

    Cambio de password base de datos

--- php/config.php
+++ php/config.php
@@ -5,7 +5,7 @@
 	//define some contstant
     define( "DB_DSN", "mysql:host=klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=rt11vry599m092xo" );
     define( "DB_USERNAME", "hs8uay923nnigqw3" );
-    define( "DB_PASSWORD", "ddtpnps8kb98bg4v" );
+    define( "DB_PASSWORD", "wllc67kxi2v8hshb" );
 	define( "CLS_PATH", "class" );
 	define( "DB_NAME", "rt11vry599m092xo" );
 	define( "DB_SERVER", "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com" );
